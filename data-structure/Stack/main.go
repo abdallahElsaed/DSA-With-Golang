143095965a759d87897c9a56bbb647be654b8231
abdalahElsaed <abdallahels2ed@gmail.com>
2024-03-20 16:47:04 +0200

    edit

--- data-structure/Stack/main.go
+++ data-structure/Stack/main.go
@@ -4,71 +4,76 @@ import (
 	"fmt"
 )
 
-type Stack struct{
+type Stack struct {
 	data []int
-	top int
+	top  int
 }
 
-func createStack(capacity int) *Stack{
+func createStack(capacity int) *Stack {
 	return &Stack{
-		data : make([]int, capacity)
-		top  : -1  
+		data: make([]int, capacity),
+		top:  -1,
 	}
 }
-/**
-	- check is the stack is full
-		- if the stack data array length == top 
-	- If not, 
-		- increase top 1 
-		- append the element in data array at top position 
+
+/*
+*
+- check is the stack is full
+  - if the stack data array length == top
+
+- If not,
+  - increase top 1
+  - append the element in data array at top position
 */
-func (s *Stack) push(value int){
-	if (len(s.data) -1) == s.top{
-		fmt.Println('your stack is full')
+func (s *Stack) push(value int) {
+	if (len(s.data) - 1) == s.top {
+		fmt.Println("your stack is full")
 	}
 	s.top++
-	append(s.data , value)
+	s.data= append(s.data, value)
 }
 
-/**
-	check if the top == -1
+/*
+*
+check if the top == -1
 */
-func (s *Stack) isEmpty()bool {
+func (s *Stack) isEmpty() bool {
 	if s.top == -1 {
-		return true 
+		return true
 	}
-	return false 
+	return false
 }
-/**
-	- Check if the data is empty
-	- If not, 
-		- take the top element value 
-		- remove the top element 
-		- decrease the top by 1
+
+/*
+*
+- Check if the data is empty
+- If not,
+  - take the top element value
+  - remove the top element
+  - decrease the top by 1
 */
-func (s *Stack) pop() int{
+func (s *Stack) pop() int {
 	if s.isEmpty() {
-		fmt.Println('your stack is empty')
+		fmt.Println("your stack is empty")
 	}
 	element := s.data[s.top]
 	s.data = s.data[:s.top]
 	s.top--
-	return element 
+	return element
 }
-func (s *stack) top(){
+func (s *Stack) getTop() {
 	fmt.Println(s.top)
 }
 
-func (s *stack) Print(){
-	for i, value :=range s.data {
-		fmt.Printf(" %d : %d " , i , value)
+func (s *Stack) print() {
+	for i, value := range s.data {
+		fmt.Printf(" %d : %d ", i, value)
 	}
 }
-func main(){
+func main() {
 	stack := createStack(3)
 	stack.push(1)
 
-	s.Print()
-
+	stack.print()
 
 }
